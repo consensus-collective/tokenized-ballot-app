bfa536089b0d915dd2d68d7eb577bd4d6ca40acb
ruggedev <ruggedev@gmail.com>
2023-08-12 03:27:57 +0800

    add scripts for TokenizedBallot

---
+++ deploy/002_TokenizedBallot.ts
@@ -0,0 +1,28 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "ethers";
+import _ from "lodash";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const proposals = _.range(10).map((i) => {
+    return ethers.encodeBytes32String("Proposal" + i);
+  });
+
+  const result = await deploy("TokenizedBallot", {
+    from: deployer,
+    args: [proposals, "0x42c9284864A50Ab5e9bE0c87220d829124Ff5d5A", 4068600],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+
+  deployments.log(
+    `Deployed TokenizedBallot: ${result.address}, Deployer: ${deployer}, network: ${hre.network.name}`
+  );
+};
+
+export default func;
+func.tags = ["TokenizedBallot"];
